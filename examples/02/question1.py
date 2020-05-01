106e73575e69f7f482e7a850cb3213546ed34a96
dondon17 <qwerqw889@ajou.ac.kr>
2020-05-01 20:20:00 +0900

    #1

---
+++ examples/02/question1.py
@@ -0,0 +1,8 @@
+score1 = 80
+score2 = 75
+score3 = 55
+print("국어: {}".format(score1))
+print("영어: {}".format(score2))
+print("수학: {}".format(score3))
+print("="*8)
+print("평균: {}".format((score1+score2+score3)/3))
