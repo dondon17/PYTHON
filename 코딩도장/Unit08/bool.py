2d1076c83724f4ab2375fa2d0157e7589b5784b0
dondon17 <qwerqw889@ajou.ac.kr>
2020-09-08 11:20:30 +0900

    update

---
+++ 코딩도장/Unit08/bool.py
@@ -0,0 +1,14 @@
+# True, False로 표현
+print(True, False)
+
+# 값 비교
+print(3>1)
+print(10 == 10)
+print(10 == 1.0)
+print(1 != 9)
+print('Python' == 'python')
+print('Python' == 'Python')
+
+# 객체 비교
+print(1 is 1.0)
+print(1 is not 1.0)
