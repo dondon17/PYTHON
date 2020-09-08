573abde7be7b201cd92f0855c5a89ca2e24fbb00
dondon17 <qwerqw889@ajou.ac.kr>
2020-09-08 11:37:42 +0900

    update

---
+++ 코딩도장/Unit09/string.py
@@ -0,0 +1,20 @@
+# 변수로 문자열 사용
+hello = "hello world"
+print(hello)
+
+# 여러 줄로 된 문자열
+hello = '''Python
+C++
+C
+JAVA'''
+print(hello)
+
+hello = """big
+quote
+same"""
+print(hello)
+
+# 따옴표 안에 따옴표
+hello = 'He said "hello"'
+print(hello)
+print('he is \'monster\'')
---
+++ 코딩도장/Unit10/list.py
@@ -0,0 +1,24 @@
+# list(배열) 만들기
+a = [34, 52, 74, 10, 4]
+print(a)
+
+# 여러 자료형을 하나의 배열로 지정 가능
+b = ['Python', 15, 3.14, True]
+print(b)
+
+# 빈 배열
+c = []
+print(c)
+c = list()
+print(c)
+
+# range활용
+print(range(10))
+d = list(range(10))
+print(d)
+d = list(range(5, 15))
+print(d)
+d = list(range(1, 100, 10))
+print(d)
+d = list(range(10, 0, -1))
+print(d)
