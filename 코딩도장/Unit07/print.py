7ad72da0a86e5c4fb0ecb4e51a6724409288ea91
dondon17 <qwerqw889@ajou.ac.kr>
2020-09-08 11:03:44 +0900

    update

--- 코딩도장/Unit07/print.py
+++ 코딩도장/Unit07/print.py
@@ -7,3 +7,11 @@ print(1, 2, 3)
 # print(변수1, 변수2, 변수3, sep = '문자 또는 문자열')
 print(1, 2, 3, sep = ', ')
 print(1920, 1080, sep = 'x')
+
+# 개행 문자 사용
+print(1, 2, 3, sep = '\n')
+
+# end 사용
+print(4, end='\n')
+print(5, end='\n')
+print(6, end='\n')
